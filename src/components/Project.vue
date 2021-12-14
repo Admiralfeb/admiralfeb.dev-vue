1e6aa131e48c55252ef199ac9b1d214a8ce19de4
Zachary Austin <zachary@admiralfeb.net>
2021-12-13 20:51:07 -0600

    add project view

---
+++ src/components/Project.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="project-header">
+    <h3 class="title">{{ project.name }}</h3>
+    <div class="fill" />
+    <a mat-button :href="project.url">Project URL</a>
+    <a mat-button :href="project.girUrl">Git URL</a>
+  </div>
+  <p>{{ project.description }}</p>
+  <h4>Technologies Used:</h4>
+  <ul>
+    <li v-for="tech of project.technologies" :key="tech">{{ tech }}</li>
+  </ul>
+  <hr />
+</template>
+
+<script lang="ts">
+import { IProject } from "@/models/project";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    project: {
+      type: Object as () => IProject,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.project-header {
+  display: flex;
+  flex-direction: row;
+}
+[mat-button] {
+  background-color: var(--primary);
+  color: white;
+  text-decoration: none;
+  margin: 10px;
+}
+h4 {
+  margin-bottom: 5px;
+}
+ul {
+  margin-top: 5px;
+}
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,15 +1,47 @@
 <template>
-  <UnderConstruction title="Projects" />
+  <div class="container">
+    <h2 class="text-center">Zachary's Projects</h2>
+    <div class="jumbotron">
+      <div class="projects" v-if="projects?.length > 0">
+        <Project
+          v-for="project in projects"
+          :project="project"
+          :key="project._id"
+        />
+      </div>
+      <div class="no-projects" v-else-if="projects?.length === 0">
+        There sees to be no projects available to view.
+      </div>
+      <Loader v-else />
+    </div>
+  </div>
 </template>
 
 <script>
 import Loader from "@/components/Loader.vue";
-import UnderConstruction from "@/components/UnderConstruction.vue";
+import Project from "@/components/Project.vue";
 import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
   components: {
-    UnderConstruction,
+    Loader,
+    Project,
+  },
+  setup() {
+    const store = useStore();
+    store.dispatch("getProjects");
+
+    return {
+      projects: computed(() => store.state.projects),
+    };
   },
 });
 </script>
+
+<style scoped>
+.jumbotron {
+  padding: 5px 10px 10px 10px;
+  width: 640px;
+}
+</style>
